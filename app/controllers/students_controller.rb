5d1b88477639dba9159c30c2d7801bb5f41f820a
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-02-02 20:26:56 +0000

    Automatically backed up by Learn

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,8 @@
+class StudentsController < ApplicationController
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  def students
+  end
+  def index
+  end
+end
